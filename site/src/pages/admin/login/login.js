3a75238f9d33afbd83993faea5a791489acc2627
Tadeu <tadeuhmc@hotemail.com>
2022-09-21 11:52:21 -0300

    Adicionando configs da api do login

--- site/src/pages/admin/login/login.js
+++ site/src/pages/admin/login/login.js
@@ -1,6 +1,50 @@
+import { loginAdmin } from '../../../../../api/src/repository/adminRepository';
+import {useNavigate} from 'react-router-dom'
+import {  useRef, useState} from 'react'
+
+
+
 import './login.scss'
 
+
+
 export default function Login () {
+
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [erro, setErro]   = useState('');
+    const[carregando, setCarregando] = useState(false);
+
+    const navegar = useNavigate();
+    const ref = useRef();
+
+    // useEffect(() => {
+    //     if(storage('usuario-logado')){
+    //     navegar('/slaa')
+    //     }
+    // }, [])
+
+    async function entrarClick(){
+        ref.current.continuousStart();
+        setCarregando(true);
+
+        try {
+            const resposta = await loginAdmin (email, senha);
+            //storage('usuario-logado', resposta)
+
+            setTimeout(() => {
+                navegar('/slaa');
+            }, 2000);
+
+        } catch (err) {
+            ref.current.complete();
+            setCarregando(false);
+
+            if(err.response.status === 401){
+                setErro(err.response.data.erro);
+            }
+        }
+    }
 
     return(     
             <div className='page-login'>
@@ -10,15 +54,19 @@ export default function Login () {
 
                     <div className='form-group'>
                         <label className='label-form' htmlFor='email'>Email</label>
-                        <input className='input-form' type='email' id='email' placeholder="" autoComplete='off'></input>
+                        <input className='input-form' type='email' id='email' placeholder="" autoComplete='off'
+                        value={email} onChange={e => setEmail(e.target.value)}/>
+                        
                     </div>
 
                     <div className='form-group'>
                         <label className='label-form' htmlFor='senha'>Senha</label>
-                        <input className='input-form' type='password' id='senha' placeholder=""></input>
+                        <input className='input-form' type='password' id='senha' placeholder=""
+                        value={senha} onChange={e => setSenha(e.target.value)}/>
                     </div>
                     
-                    <button className='btn-login'>Entrar</button>
+                    <button className='btn-login' onClick={entrarClick}
+                 disabled={carregando}>Entrar</button>
 
                 </form>
             </div>
